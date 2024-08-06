ca237bd4ed3f1e66ade2f789ffb5250c0abbaf23
sudhar224 <sudharsankarthik224@gmail.com>
2024-08-06 20:31:52 +0530

    cart controller added

---
+++ ShoppingMartWithoutMVC/Controllers/CartController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ShoppingMartWithoutMVC.Models;
+
+namespace ShoppingMartWithoutMVC.Controllers
+{
+    public class CartController : Controller
+    {
+        string constr = ConfigurationManager.ConnectionStrings["Dbconnection"].ConnectionString;
+        private static List<CartItem> Cart = new List<CartItem>();
+
+        // GET: Cart
+        public ActionResult Index()
+        {
+            return View(Cart);
+        }
+
+        // POST: Cart/Add
+        [HttpPost]
+        public ActionResult AddToCart(int id, int quantity)
+        {
+            var product = GetProductById(id); // Implement this method to fetch product details by ID
+            if (product != null)
+            {
+                var cartItem = Cart.FirstOrDefault(c => c.Product.id == id);
+                if (cartItem != null)
+                {
+                    cartItem.Quantity += quantity;
+                }
+                else
+                {
+                    Cart.Add(new CartItem
+                    {
+                        Product = product,
+                        Quantity = quantity
+                    });
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+        private AddProduct GetProductById(int id)
+        {
+            AddProduct addProduct_obj = new AddProduct();
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                SqlCommand cmd = new SqlCommand("sp_product_details " + id, con);
+                con.Open();
+                SqlDataReader sdr = cmd.ExecuteReader();
+                while (sdr.Read())
+                {
+                    addProduct_obj = new AddProduct
+                    {
+                        img_path = sdr["img_path"].ToString(),
+                        product_id = Convert.ToInt32(sdr["product_id"].ToString()),
+                        product_name = sdr["product_name"].ToString(),
+                        price = sdr["price"].ToString()
+                    };
+                }
+                sdr.Close();
+                con.Close();
+            }
+            //return View(addProduct_obj);
+            return (addProduct_obj);
+            //{
+            //    id = id,
+            //    product_id = id,
+            //    product_name = "Sample Product", // Replace with actual product name
+            //    price = "100", // Replace with actual price
+            //    img_path = "path/to/image" // Replace with actual image path
+            //};
+        }
+
+        [HttpPost]
+        public ActionResult Remove(int id)
+        {
+            var itemToRemove = Cart.FirstOrDefault(c => c.Product.id == id);
+            if (itemToRemove != null)
+            {
+                Cart.Remove(itemToRemove);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ShoppingMartWithoutMVC/Models/CartItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ShoppingMartWithoutMVC.Models;
+
+namespace ShoppingMartWithoutMVC.Models
+{
+    public class CartItem
+    {
+        public AddProduct Product { get; set; } // Details of the product
+        public int Quantity { get; set; } // Quantity of the product in the cart
+    }
+}
